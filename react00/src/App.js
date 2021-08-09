79ce8df87619810a6232dbac61c011d98853475f
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-08 22:51:32 -0500

    0ne

---
+++ react00/src/App.js
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello React</h1>
+      <p>React is a library which is used for UI , it is js library</p>
+    </div>
+  );
+}
+
+export default App;
