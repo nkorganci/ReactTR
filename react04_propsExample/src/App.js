63ab9450c94bdfcfed2a37e1330c2601a88ed536
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-10 13:09:35 -0500

    a

--- react04_propsExample/src/App.js
+++ react04_propsExample/src/App.js
@@ -1,10 +1,13 @@
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
 import Yorum from "./components/yorum/Yorum";
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNav from "./components/nav/MyNav";
 
 function App() {
   return (
     <div className="App">
+      <MyNav />
       <Header />
       <div className="container-yorum">
         <Yorum index="0" />
