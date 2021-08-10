050c0963bbd69372250119edd262dbbdbf9335e4
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-10 13:28:41 -0500

    b

---
+++ react06_react-bootstrap/src/App.js
@@ -0,0 +1,15 @@
+import MyNav from "./MyNav";
+import Main from "./Main";
+import Footer from "./Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <MyNav />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+export default App;
