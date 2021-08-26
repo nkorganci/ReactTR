f00e525d9660bb73af0ae9ad623ee2e9378417c7
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-26 15:25:35 -0500

    a

--- react04_propsExample/src/components/yorum/yorum.js
+++ react04_propsExample/src/components/yorum/yorum.js
@@ -2,15 +2,17 @@ import React from "react";
 import yorumlar from "./data.js";
 import "./Yorum.css";
 function Yorum(props) {
-  const {ad, meslek, resim, yorum} = yorumlar[props.index]
+  const {id,ad, meslek, resim, yorum} = yorumlar[props.index]
 
   // console.log(yorumlar[props.index]); we can see values in console
   return (
     <div className="yorum-div">
+      <h1> {id} </h1>
       <h1> {ad} </h1>
       <h2>{meslek}</h2>
       <img src={resim} alt="" />
       <h3>{yorumlar[props.index].ad}</h3>
+      {/* Longer Version */}
     </div>
   );
 }
