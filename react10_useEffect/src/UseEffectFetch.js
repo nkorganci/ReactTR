24444412b1f1ddb990beb1551fbe37c0864af791
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-28 14:37:05 -0500

    a

--- react10_useEffect/src/UseEffectFetch.js
+++ react10_useEffect/src/UseEffectFetch.js
@@ -1,4 +1,3 @@
-
 //* GET API FROM RESTCOUNTRIES
 import React, { useState, useEffect, version } from "react";
 
@@ -10,28 +9,29 @@ function UseEffectFetch() {
 
   useEffect(() => {
     fetch("https://restcountries.eu/rest/v2/all")
-    .then((res) => res.json())
-    .then((data) => setUlkeler(data));
+      .then((res) => res.json())
+      .then((data) => setUlkeler(data));
     // .catch((err) => console.log(err));
   }, []);
   // [] means render just one time
 
-
   return (
     <div>
-    <h1>ULKE BILGILERI</h1>
-      {ulkeler.map((ulke)=>{
+      <h1>ULKE BILGILERI</h1>
+      {ulkeler.map((ulke) => {
         return (
-          <div className="d-flex justify-content-center text-center mt-5 bg-warning" >
+          <div
+            key={ulke.id}
+            className="d-flex justify-content-center text-center mt-5 bg-warning"
+          >
             <img src={ulke.flag} alt="" width="20%" />
             <p>{ulke.name}</p>
             <h1>{ulke.capital} </h1>
           </div>
-        )
+        );
       })}
     </div>
-
-  )
+  );
 }
 
 export default UseEffectFetch;
