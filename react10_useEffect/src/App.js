197c68f5336b077807707cb7ac3668df097aa52d
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-28 13:20:55 -0500

    c

--- react10_useEffect/src/App.js
+++ react10_useEffect/src/App.js
@@ -8,8 +8,8 @@ function App() {
   return (
     <div className="App">
       {/* <UseEffect01 /> */}
-      <UseEffectFetch />
-      {/* <UseEffectFetchError /> */}
+      {/* <UseEffectFetch /> */}
+      <UseEffectFetchError />
       {/* <UseEffectAxios /> */}
       {/* <UseEffectFetchTxt /> */}
     </div>
--- react10_useEffect/src/UseEffectFetch.js
+++ react10_useEffect/src/UseEffectFetch.js
@@ -1,16 +1,37 @@
+
+//* GET API FROM RESTCOUNTRIES
 import React, { useState, useEffect, version } from "react";
 
 function UseEffectFetch() {
   //This is a GET method, if you want to update or put, you need to write "post"...in addres with comma
   //Fetch method return data in this function, so useState need to be assigned.
-  
-  const[ulkeler,setUlkeler]=useState([]);
-  fetch("https://restcountries.eu/rest/v2/all")
+
+  const [ulkeler, setUlkeler] = useState([]);
+
+  useEffect(() => {
+    fetch("https://restcountries.eu/rest/v2/all")
     .then((res) => res.json())
     .then((data) => setUlkeler(data));
-    .catch((err) => console.log(err));
+    // .catch((err) => console.log(err));
+  }, []);
+  // [] means render just one time
 
-  return <div></div>;
+
+  return (
+    <div>
+    <h1>ULKE BILGILERI</h1>
+      {ulkeler.map((ulke)=>{
+        return (
+          <div className="d-flex justify-content-center text-center mt-5 bg-warning" >
+            <img src={ulke.flag} alt="" width="20%" />
+            <p>{ulke.name}</p>
+            <h1>{ulke.capital} </h1>
+          </div>
+        )
+      })}
+    </div>
+
+  )
 }
 
 export default UseEffectFetch;
