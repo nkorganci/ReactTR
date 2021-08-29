91d87171de06263215254053f27b52e7bc8c1a5d
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-28 22:25:32 -0500

    a

--- react10_useEffect/src/App.js
+++ react10_useEffect/src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <div className="App">
       {/* <UseEffect01 /> */}
-      {/* <UseEffectFetch /> */}
+      <UseEffectFetch />
       {/* <UseEffectFetchError /> */}
-      <UseEffectAxios />
+      {/* <UseEffectAxios /> */}
       {/* <UseEffectFetchTxt /> */}
     </div>
   );
