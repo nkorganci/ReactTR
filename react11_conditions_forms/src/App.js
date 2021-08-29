f6f3b8066f32d97aefc72357de7ba5b89375435b
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-29 16:11:48 -0500

    a

--- react11_conditions_forms/src/App.js
+++ react11_conditions_forms/src/App.js
@@ -4,9 +4,9 @@ import Form02 from "./Form02";
 function App() {
   return (
     <div className="App">
-      <ConditionalRender />
-      {/* <Form01 /> */}
-      <Form02 />
+      {/* <ConditionalRender /> */}
+      <Form01 />
+      {/* <Form02 /> */}
     </div>
   );
 }
--- react11_conditions_forms/src/ConditionalRender.js
+++ react11_conditions_forms/src/ConditionalRender.js
@@ -36,34 +36,30 @@ const Footer = () => {
 
 function ConditionalRender() {
   const [goster, setGoster] = useState(false);
-  console.log(goster);
+
   function gosterGizle() {
     setGoster(!goster);
   }
+  console.log(goster);
 
   return (
-    <div className="text-center">
+    <div className="text-center m-5">
       <button onClick={gosterGizle} className="btn btn-warning m-4 p-3">
         goster/gizle
       </button>
-      {/* Shorcut */}
-      {/* 1. degisken true ise 2.yi calitirir. */}
-      {goster && <Kart />}
-      {/* 1. degisken false ise 2.yi calistirir. */}
+      {/* {goster && <Kart />} */}
+      {/* 1.degisken dogru ise goster karti */}
+      {/* !goster && <Footer /> */}
+      {/* 1.degisken dogru is "goster", yoksa "kart" */}
       {/* {goster || <Kart />} */}
-      {!goster && <Footer />}
-      {/* Ternary yontemi */}
-      {goster ? (
+      {goster ? ( 
         <div>
-          <h1>Kart modulu gozukuyor</h1>
-          <Kart />
-        </div>
-      ) : (
-        <>
-          <h1>Footer modulu gozukuyor</h1>
-          <Footer />
-        </>
-      )}
+          <h1>Kart modulu gosteriliyor</h1>  <Kart /> </div>) 
+          : 
+          (<div>
+         <h1>Footer is shown</h1>
+         <Footer />
+       </div> )}
     </div>
   );
 }
