50ed87964fcd31133240dfb8ae9e3726b64c43a2
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-31 08:25:25 -0500

    a

---
+++ react13_context/src/App.js
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+import data from "./data";
+
+function App() {
+  const [kisiler, setKisiler] = useState();
+  return <div></div>;
+}
+
+export default App;
