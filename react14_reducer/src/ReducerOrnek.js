274a57cd8c236a6f1ede8a335362ba6209268102
Nuri Korganci <75337382+nkorganci@users.noreply.github.com>
2021-08-31 14:21:44 -0500

    Reducer

--- react14_reducer/src/ReducerOrnek.js
+++ react14_reducer/src/ReducerOrnek.js
@@ -1,33 +1,60 @@
-import React, { useState } from "react";
-
+import React, { useState, useReducer } from "react";
+import { reducer } from "./reducer";
 const ReducerOrnek = () => {
-  const [yukleniyor, setYukleniyor] = useState(false);
-  const [resim, setResim] = useState("");
-  const [hata, setHata] = useState("");
+  //Reducer is used so we dont need this
+  // const [yukleniyor, setYukleniyor] = useState(false);
+  // const [resim, setResim] = useState("");
+  // const [hata, setHata] = useState("");
+
+  //? USEREDUCER Bir çok state'in tek bir elden kontrol edilmesine izin veren bir state yönetim Hook'udur.
+  //* - useReducer 2 parametre alır.
+  //*     1) Reducer metodu : State'lerin alacağı değerlerin atandığı metot.
+  //*     2) State'lerin başlangıç durumları
+  //* - 2 adet dönüş değeri vardır.
+  //*   State'ler ve bu state'lere ait olan işlemler (action dispatch)
+
+  //* Tüm state'lerin başlangıç durumlarının tanımlanamasi
+  const initialState = {
+    yukleniyor: false,
+    resim: "",
+    hata: "",
+  };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { resim, yukleniyor, hata } = state;
 
   const getir = () => {
-    setResim("");
+    /*     setResim("");
     setHata("");
-    setYukleniyor(true);
+    setYukleniyor(true); */
+    dispatch({ type: "GETIR_BASLA" });
 
     fetch("https://dog.ceo/api/breeds/image/random")
-    .then((res) => res.json())
-    .then((data)=>{
-      setYukleniyor(false);
-      setResim(data.message);
-    }).catch(()={ 
-      setYukleniyor(false);
-      setHata("Hata veriler cekilemedi");
-    })
+      .then((res) => res.json())
+      .then((data) => {
+        /*     setYukleniyor(false);
+        setResim(data.message); */
+        dispatch({ type: "GETIR_BASARILI", payload: data.message });
+      })
+      .catch(() => {
+        /*    setYukleniyor(false);
+        setHata("Hata veriler cekilemedi"); */
+        dispatch({ type: "GETIR_HATA", payload: "Hata veriler cekilemedi" });
+      });
   };
-
-
 
   return (
     <div>
       <button onClick={getir} disable={yukleniyor}>
         GETIR
       </button>
+
+      {resim && (
+        <div>
+          <img src={resim} width="500px" alt="" />
+        </div>
+      )}
+
+      {hata && <p>{hata}</p>}
     </div>
   );
 };
--- react14_reducer/src/reducer.js
+++ react14_reducer/src/reducer.js
@@ -4,7 +4,7 @@ export const reducer = (state, action) => {
       return { ...state, resim: "", hata: "", yukleniyor: true };
     case "GETIR_BASARILI":
       return { ...state, resim: action.payload, hata: "", yukleniyor: false };
-    case "GETIR_HATA":
-      return { ...state, resim: "", hata: action.payload, yukleniyor: false };
+    case "Hata":
+      return { ...state, resim: "action", hata: action.payload, yukleniyor: false };
   }
 };
